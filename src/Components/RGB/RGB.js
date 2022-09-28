ee2d91001707304f59ca41ce7eba66ea57c192b1
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-09-27 17:34:55 -0700

    added export function RGB to RGB.js

---
+++ src/Components/RGB/RGB.js
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+import './RGB.css';
+
+export default function RGB() {
+  const { r, g, b } = useParams();
+  const currentColor = `rgb(${r},$}{g},${b})`;
+
+  return (
+    <div key={Math.random()} style={{ backgroundColor: currentColor }} className="colors">
+      <h1>{`rgb(${r},${g},${b})`}</h1>
+    </div>
+  );
+}
